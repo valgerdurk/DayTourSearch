ad55659c69ee341a5c531eecd60978cdb81af372
David Francis <dff1@hi.is>
2019-03-29 13:56:24 +0000

    Added Test Fixture for InfoView

---
+++ DayTours/test/DayTour/InfoViewTest.java
@@ -0,0 +1,103 @@
+/*
+ * This source code is provided as-is.  No responsibility is taken
+ *  for its usage or maintenance.  All queries should be directed
+ *  to the author.
+ */
+package DayTour;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author David Francis <dff1@hi.is>
+ */
+public class InfoViewTest {
+    
+    public InfoViewTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of init method, of class InfoView.
+     */
+    @Test
+    public void testInit() {
+    }
+
+    /**
+     * Test of SearchByType method, of class InfoView.
+     */
+    @Test
+    public void testSearchByType() {
+    }
+
+    /**
+     * Test of SearchByText method, of class InfoView.
+     */
+    @Test
+    public void testSearchByText() {
+    }
+
+    /**
+     * Test of SearchByRegion method, of class InfoView.
+     */
+    @Test
+    public void testSearchByRegion() {
+    }
+
+    /**
+     * Test of SearchByPrice method, of class InfoView.
+     */
+    @Test
+    public void testSearchByPrice() {
+    }
+
+    /**
+     * Test of SearchByDuration method, of class InfoView.
+     */
+    @Test
+    public void testSearchByDuration() {
+    }
+
+    /**
+     * Test of SortByRating method, of class InfoView.
+     */
+    @Test
+    public void testSortByRating() {
+    }
+
+    /**
+     * Test of SortByPrice method, of class InfoView.
+     */
+    @Test
+    public void testSortByPrice() {
+    }
+
+    /**
+     * Test of SortByDuration method, of class InfoView.
+     */
+    @Test
+    public void testSortByDuration() {
+    }
+    
+}
